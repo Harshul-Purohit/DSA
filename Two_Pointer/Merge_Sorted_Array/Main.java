e5e28c6cc7b33cd8dff9a2cb5f249e040ed36f44
Harshul-Purohit <harshulpurohit@gmail.com>
2026-07-01 18:01:40 +0530

    Merge_Sorted_Array soultion

---
+++ Two_Pointer/Merge_Sorted_Array/Main.java
@@ -0,0 +1,49 @@
+package Merge_Sorted_Array;
+
+import java.util.Arrays;
+
+public class Main {
+  public static void main(String[] args) {
+  int[] nums1 = {1, 2, 3, 0, 0, 0};
+int m = 3;
+
+int[] nums2 = {2, 5, 6};
+int n = 3;
+
+
+int i = m-1;
+int j = n-1;
+int k = m+n-1;
+
+// // Append nums2 into nums1 starting at index m
+// for (int i = 0; i < n; i++) {
+//     nums1[m + i] = nums2[i];
+// }
+
+// Arrays.sort(nums1);
+
+// System.out.println(Arrays.toString(nums1));
+
+
+    while(i >= 0 && j >= 0){
+      if(nums1[i]>nums2[j]){
+        nums1[k]=nums1[i];
+        i--;
+      }else{
+        nums1[k]=nums2[j];
+        j--;
+      }
+      k--;
+    }
+
+    while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+}
+
+
+    System.out.println(Arrays.toString(nums1));
+  }
+  
+}
