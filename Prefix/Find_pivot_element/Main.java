76aa5ea3e31c329f8f62c5d90dbbf069a680a0ef
Harshul-Purohit <harshulpurohit@gmail.com>
2026-07-18 19:14:01 +0530

    Find_pivot_element solution

--- Prefix/Find_pivot_element/Main.java
+++ Prefix/Find_pivot_element/Main.java
@@ -3,33 +3,29 @@ package Find_pivot_element;
 public class Main {
 
 
-    public int pivotIndex(int[] nums) {
+  public static void main(String[] args) {
+    int[] nums = {1,7,3,6,5,6};
+    int totalsum = 0;
 
-        int n = nums.length;
 
-        int totalSum = 0;
+for(int num: nums){
+totalsum+=num;
+}
 
-        // Calculate total sum
-        for(int num : nums) {
-            totalSum += num;
-        }
+int leftsum = 0;
 
-        int leftSum = 0;
+for(int i=0; i<nums.length;i++){
+    int rightsum = totalsum-leftsum-nums[i];
 
-        for(int i = 0; i < n; i++) {
-
-            int rightSum = totalSum - leftSum - nums[i];
-
-            // Check pivot condition
-            if(leftSum == rightSum) {
-                return i;
-            }
-
-            // Move current element to left side
-            leftSum += nums[i];
-        }
-
-        return -1;
+    if(rightsum==leftsum){
+        System.out.println(i);
+        break;
     }
+
+    leftsum+=nums[i];
+
+}
+
+  }
 }
 
