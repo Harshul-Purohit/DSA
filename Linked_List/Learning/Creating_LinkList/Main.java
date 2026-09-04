5c2499db0082f763da9440086b607454e0d4c2c7
Harshul-Purohit <harshulpurohit@gmail.com>
2026-09-04 15:30:05 +0530

    creating and traversing linkedlist

---
+++ Linked_List/Learning/Creating_LinkList/Main.java
@@ -0,0 +1,27 @@
+package Learning.Creating_LinkList;
+
+class Node{
+  int data;
+  Node next;
+
+  Node(int new_data){
+    this.data = new_data;
+    this.next = null;
+  }
+}
+
+public class Main {
+  public static void main(String[] args){
+
+    Node  head = new Node(10);
+    head.next = new Node(20);
+    head.next.next = new Node(30);
+    head.next.next.next = new Node(10);
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+  }
+}
